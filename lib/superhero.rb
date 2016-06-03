620293dcd2fc7c8f4c329a9b5c6f399efa9d2dac
Mary Rose Cook <mary@maryrosecook.com>
2016-06-03 11:20:49 +0100

    add fighting

--- lib/superhero.rb
+++ lib/superhero.rb
@@ -1,11 +1,15 @@
 module UsesPower
+  attr_reader :power
+
   def use_power
-    "#{ @name } is now #{ @power }"
+    "#{ @name } is now #{ power.description }"
   end
 end
 
 class Superhero
   include UsesPower
+
+  attr_reader :name
 
   def initialize(name, power)
     @name = name
@@ -15,10 +19,16 @@ class Superhero
   def use_power
     p "#{ super }!"
   end
+
+  def >(other)
+    power > other.power
+  end
 end
 
 class Villain
   include UsesPower
+
+  attr_reader :name
 
   def initialize(name, power)
     @name = name
@@ -30,23 +40,56 @@ class Villain
   end
 end
 
+class Power
+  attr_reader :description, :rating
+  protected :rating
 
+  def initialize(description, rating)
+    @description = description
+    @rating = rating
+  end
+
+  def >(other)
+    rating > other.rating
+  end
+end
+
+class Fight
+  def initialize(hero, villain)
+    @hero = hero
+    @villain = villain
+  end
+
+  def run
+    p hero.use_power
+    p villain.use_power
+    p "#{winner.name} is victorious!"
+  end
+
+  private
+
+  attr_reader :hero, :villain
+
+  def winner
+    hero > villain ? hero : villain
+  end
+end
 
 speed = Power.new("running real fast", 3)
+wealth_and_sadness = Power.new("wealthy and sad :*(", -10)
 
 flash = Superhero.new("The Flash", speed)
-flash.use_power # => "The Flash is now running real fast!"
+# flash.use_power # => "The Flash is now running real fast!"
 
 santa = Superhero.new("Santa Claus", speed)
-santa.use_power # => "Santa Claus is now running real fast!"
+# santa.use_power # => "Santa Claus is now running real fast!"
 
-batman = Superhero.new("Batman", "wealthy and sad :(")
-batman.use_power # => "Batman is now wealthy and sad :(!"
+batman = Superhero.new("Batman", wealth_and_sadness)
+# batman.use_power # => "Batman is now wealthy and sad :(!"
 
-lex = Villain.new("Lex Luthor", "wealthy and sad :(")
-lex.use_power # => "Lex is now wealthy and sad :(, villainously!"
+lex = Villain.new("Lex Luthor", wealth_and_sadness)
+# lex.use_power # => "Lex is now wealthy and sad :(, villainously!"
 
-flash > lex # => true
+p flash > lex # => true
 
 Fight.new(flash, lex).run # => "The Flash is now running real fast! Lex is now wealthy and sad :(, villainously! The Flash is victorious!"
-
